0d011c49731ccc15950e99759c883e6da160b1b0
Raszbanana <niko.hochloff@gmail.com>
2022-02-20 19:35:57 +0100

    Added some API data

--- 3_World_Wide_Express/app.js
+++ 3_World_Wide_Express/app.js
@@ -2,13 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send(
-        `<h1>Welcome to my website</h1>
-         <p>SOMETHING</p>
-         <div>Contagious</div>`);
+    res.sendFile(__dirname + "/public/weather.html")    
 })
-
-// fetch('insertWeatherForeCastApi')
 
 app.get("/forecast", (req, res) => {
     res.sendFile(__dirname + "/public/weather.html")
